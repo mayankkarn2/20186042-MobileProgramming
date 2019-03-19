4bddb16ce20a1378d92115080e56cc9d3c6b3d61
mayankkarn2 <mayankkarn2@msitprogram.net>
2019-03-19 11:17:39 +0530

    Module-5

---
+++ Module-5/Activity-23/T10.04-Exercise-PeriodicSyncWithJobDispatcher 2/app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.background.sync;
+
+public class WaterReminderFirebaseJobService {
+    // TODO3 (3) WaterReminderFirebaseJobService should extend from JobService
+
+    // TODO4 (4) Override onStartJob
+        // TODO5 (5) By default, jobs are executed on the main thread, so make an anonymous class extending
+        //  AsyncTask called mBackgroundTask.
+            // TODO6 (6) Override doInBackground
+                // TODO7 (7) Use ReminderTasks to execute the new charging reminder task you made, use
+                // this service as the context (WaterReminderFirebaseJobService.this) and return null
+                // when finished.
+            // TODO8 (8) Override onPostExecute and call jobFinished. Pass the job parameters
+            // and false to jobFinished. This will inform the JobManager that your job is done
+            // and that you do not want to reschedule the job.
+
+
+
+}
